eaae925e7edccc2ab653217cece09e43da8565da
Kento <kento.nishida.arch@gmail.com>
2024-04-23 12:01:38 +0900

    update Like

--- app/Http/Controllers/likeController.php
+++ app/Http/Controllers/likeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Like;
 
-class likeController extends Controller
+class LikeController extends Controller
 {
     private $like;
 
